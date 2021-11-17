206d7be2e0578cc4348cc0e0ec6037dcf0f60d90
Federico Becoña <41715911+federicobecona@users.noreply.github.com>
2021-11-17 23:23:49 +0000

    backend

--- src/app.js
+++ src/app.js
@@ -1,13 +1,57 @@
 var express = require('express');
 
 var app = express();
+app.use(express.json()) 
 
-app.get('/hola-mundo', function(req, res) {
-  res.send("¡Hola mundo!")
+app.get('/experiencia-laboral', function(req, res) {
+  let expLab = [
+    {
+      "empresa":"...",
+      "puesto":"...",
+      "descripcion":"...",
+      "fechaInicio": new Date("2021-04-23"),
+      "puesto": new Date("2015-07-04")
+    },
+    {
+      "empresa":"...",
+      "puesto":"...",
+      "descripcion":"...",
+      "fechaInicio": new Date("2021-04-23"),
+      "puesto": new Date("2015-07-04")
+    },
+    {
+      "empresa":"...",
+      "puesto":"...",
+      "descripcion":"...",
+      "fechaInicio": new Date("2021-04-23"),
+      "puesto": new Date("2015-07-04")
+    }
+  ]
+  res.send(expLab)
 });
 
-app.listen(process.env.PORT || 3000, (a) => {
-  console.log("Servidor disponible en http://localhost:3000")
+app.post('/hacer-cookie', (req, res) => {
+  if(req.body.nombreContacto != null){
+    res.cookie(`PW_2021-CV_Contacto`,JSON.stringify(
+    {nombreContacto: req.body.nombreContacto}), {
+      secure: true,
+      httpOnly: true,
+    });
+    res.status(200);
+    res.send("Se ha creado satisfactoriamente");
+  }else{
+    res.status(400);
+    res.send('Falta el nombre de contacto');
+  }
+});
+
+app.use(function(req,res){
+    res.status(404);
+    res.send('404 no fue encontrado');
+});
+
+app.listen(process.env.PORT || 4000, (a) => {
+  console.log("Servidor disponible en http://localhost:4000")
 });
  
 module.exports = app;
