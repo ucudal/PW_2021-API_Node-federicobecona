293d8d4c9faf0f8e7ce12f9018797d7548f325dc
Federico Becoña <41715911+federicobecona@users.noreply.github.com>
2021-11-19 02:59:45 +0000

    get y post

--- src/app.js
+++ src/app.js
@@ -1,57 +1,71 @@
 var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 var app = express();
-app.use(express.json()) 
+app.use(cookieParser());
+
+app.use(cors({
+    origin: '*'
+}));
+
+app.use(express.urlencoded({
+  extended: true
+}));
+
+var jsonParser = bodyParser.json();
+
+
 
 app.get('/experiencia-laboral', function(req, res) {
-  let expLab = [
+  res.send([
     {
-      "empresa":"...",
-      "puesto":"...",
-      "descripcion":"...",
-      "fechaInicio": new Date("2021-04-23"),
-      "puesto": new Date("2015-07-04")
+      "empresa":"Tesla",
+      "puesto":"CEO",
+      "descripcion":"Hago autos eléctricos",
+      "fechaInicio": new Date("2015-07-04"),
+      "fechaFin": new Date("2015-07-04")
     },
     {
-      "empresa":"...",
-      "puesto":"...",
-      "descripcion":"...",
-      "fechaInicio": new Date("2021-04-23"),
-      "puesto": new Date("2015-07-04")
+      "empresa":"SpaceX",
+      "puesto":"CEO",
+      "descripcion":"Hago cohetes espaciales",
+      "fechaInicio": new Date("2015-07-04"),
+      "fechaFin": new Date("2015-07-04")
     },
     {
-      "empresa":"...",
-      "puesto":"...",
-      "descripcion":"...",
+      "empresa":"SolarCity",
+      "puesto":"CEO",
+      "descripcion":"Diseño paneles solares",
       "fechaInicio": new Date("2021-04-23"),
-      "puesto": new Date("2015-07-04")
-    }
-  ]
-  res.send(expLab)
+      "fechaFin": new Date("2015-07-04")
+    } 
+  ]);
 });
 
-app.post('/hacer-cookie', (req, res) => {
-  if(req.body.nombreContacto != null){
-    res.cookie(`PW_2021-CV_Contacto`,JSON.stringify(
-    {nombreContacto: req.body.nombreContacto}), {
+app.post('/hacer-cookie', jsonParser, (req, res) => {
+  let nombre = req.body.nombreContacto;
+  if(!nombre){
+    res.status(400).send('Falta el nombre de contacto');
+  }else{
+    res.status(200).cookie(`PW_2021-CV_Contacto`,JSON.stringify(
+    {nombreContacto: nombre}), {
       secure: true,
       httpOnly: true,
-    });
-    res.status(200);
-    res.send("Se ha creado satisfactoriamente");
-  }else{
-    res.status(400);
-    res.send('Falta el nombre de contacto');
+    }).send("Se ha enviado satisfactoriamente");
   }
 });
+
+
+
+
 
 app.use(function(req,res){
     res.status(404);
     res.send('404 no fue encontrado');
 });
-
-app.listen(process.env.PORT || 4000, (a) => {
-  console.log("Servidor disponible en http://localhost:4000")
+app.listen(process.env.PORT || 3000, (a) => {
+  console.log("Servidor disponible en http://localhost:3000")
 });
- 
 module.exports = app;
